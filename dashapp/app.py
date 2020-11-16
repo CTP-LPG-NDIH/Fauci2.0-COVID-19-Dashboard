92e8ea3865aca7ec6a19b806738cc6f333844ba4
ndropp92 <ndropp92@aol.com>
2020-11-16 13:59:52 -0500

    adding dashapp code

---
+++ dashapp/app.py
@@ -0,0 +1,117 @@
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv('/Users/chris.christie92/Fauci2.0s/COVID-19-Dashboard/dashapp/covid_data/covid-19-data/live/us-states.csv')
+
+fig = px.bar(df, x="state", y="cases", color="state")
+
+app.layout = html.Div(children=[
+    html.H1(children='Covid19 dashboard'),
+
+    html.Div(children='''
+        State By State Cases
+    '''),
+    html.Label('State Dropdown'),
+    dcc.Dropdown(
+    	id='dropdown-1',
+        options=[
+       		{'label': 'Alabama','value': 'Alabama'},
+			{'label': 'Alaska','value': 'Alaska'},
+			{'label': 'Arizona','value': 'Arizona'},
+			{'label': 'Arkansas','value': 'Arkansas'},
+			{'label': 'California','value': 'California'},
+			{'label': 'Colorado','value': 'Colorado'},
+			{'label': 'Connecticut','value': 'Connecticut'},
+			{'label': 'Delaware','value': 'Delaware'},
+			{'label': 'District of Columbia','value': 'District of Columbia'},
+			{'label': 'Florida','value': 'Florida'},
+			{'label': 'Georgia','value': 'Georgia'},
+			{'label': 'Guam','value': 'Guam'},
+			{'label': 'Hawaii','value': 'Hawaii'},
+			{'label': 'Idaho','value': 'Idaho'},
+			{'label': 'Illinois','value': 'Illinois'},
+			{'label': 'Indiana','value': 'Indiana'},
+			{'label': 'Iowa','value': 'Iowa'},
+			{'label': 'Kansas','value': 'Kansas'},
+			{'label': 'Kentucky','value': 'Kentucky'},
+			{'label': 'Louisiana','value': 'Louisiana'},
+			{'label': 'Maine','value': 'Maine'},
+			{'label': 'Maryland','value': 'Maryland'},
+			{'label': 'Massachusetts','value': 'Massachusetts'},
+			{'label': 'Michigan','value': 'Michigan'},
+			{'label': 'Minnesota','value': 'Minnesota'},
+			{'label': 'Mississippi','value': 'Mississippi'},
+			{'label': 'Missouri','value': 'Missouri'},
+			{'label': 'Montana','value': 'Montana'},
+			{'label': 'Nebraska','value': 'Nebraska'},
+			{'label': 'Nevada','value': 'Nevada'},
+			{'label': 'New Hampshire','value': 'New Hampshire'},
+			{'label': 'New Jersey','value': 'New Jersey'},
+			{'label': 'New Mexico','value': 'New Mexico'},
+			{'label': 'New York','value': 'New York'},
+			{'label': 'North Carolina','value': 'North Carolina'},
+			{'label': 'North Dakota','value': 'North Dakota'},
+			{'label': 'Northern Mariana Islands','value': 'Northern Mariana Islands'},
+			{'label': 'Ohio','value': 'Ohio'},
+			{'label': 'Oklahoma','value': 'Oklahoma'},
+			{'label': 'Oregon','value': 'Oregon'},
+			{'label': 'Pennsylvania','value': 'Pennsylvania'},
+			{'label': 'Puerto Rico','value': 'Puerto Rico'},
+			{'label': 'Rhode Island','value': 'Rhode Island'},
+			{'label': 'South Carolina','value': 'South Carolina'},
+			{'label': 'South Dakota','value': 'South Dakota'},
+			{'label': 'Tennessee','value': 'Tennessee'},
+			{'label': 'Texas','value': 'Texas'},
+			{'label': 'Utah','value': 'Utah'},
+			{'label': 'Vermont','value': 'Vermont'},
+			{'label': 'Virginia','value': 'Virginia'},
+			{'label': 'Virgin Islands','value': 'Virgin Islands'},
+			{'label': 'Washington','value': 'Washington'},
+			{'label': 'West Virginia','value': 'West Virginia'},
+			{'label': 'Wisconsin','value': 'Wisconsin'},
+			{'label': 'Wyoming','value': 'Wyoming'}
+            
+
+        ],
+        value=['New York'],
+        multi=True
+    ),
+    dcc.Graph(
+        id='state-cases-bar-graph',
+        figure=fig
+    )
+])
+
+#callbacks
+
+@app.callback(
+    Output('state-cases-bar-graph', 'figure'),
+    [Input('dropdown-1', 'value')])
+def update_figure(selected_states):
+	#print((selected_states))
+	new_df = pd.DataFrame()
+	for state in selected_states:
+		to_append = pd.DataFrame(df[df.state == state])
+		new_df = new_df.append(to_append)
+
+	
+	fig = px.bar(new_df, x="state", y="cases", color="state")
+	fig.update_layout(transition_duration=500)
+	return fig
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
