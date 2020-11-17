0c282ad1d03b9da5ab041aa522f2e7e024a8c761
ndropp92 <ndropp92@aol.com>
2020-11-16 20:54:54 -0500

    finished interactive graph

--- dashapp/app.py
+++ dashapp/app.py
@@ -1,4 +1,3 @@
-
 #from data import *
 import dash
 import dash_core_components as dcc
@@ -15,7 +14,40 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_csv('/Users/chris.christie92/Fauci2.0s/Fauci2.0-COVID-19-Dashboard/data/us-states.csv')
 
-fig = px.bar(df, x="state", y="cases", color="state")
+import plotly.graph_objects as go
+
+
+
+fig = go.Figure()
+fig.add_trace(go.Bar(x=df['state'],
+                y=df['cases'],
+                name='Cases by State',
+                marker_color='rgb(55, 83, 109)'
+                ))
+fig.add_trace(go.Bar(x=df['state'],
+                y=df['deaths'],
+                name='Deaths by State',
+                marker_color='rgb(26, 118, 255)'
+                ))
+
+fig.update_layout(
+    title='Cases and Death Count Per State'
+    ,xaxis_tickfont_size=14,
+    yaxis=dict(
+        title='State',
+        titlefont_size=16,
+        tickfont_size=14,
+    ),
+    legend=dict(
+       
+        bgcolor='rgba(255, 255, 255, 0)',
+        bordercolor='rgba(255, 255, 255, 0)'
+    ),
+    barmode='group',
+    bargap=0.15, # gap between bars of adjacent location coordinates.
+    bargroupgap=0.1 # gap between bars of the same location coordinate.
+)
+
 
 app.layout = html.Div(children=[
     html.H1(children='Covid19 dashboard'),
@@ -106,8 +138,38 @@ def update_figure(selected_states):
 		to_append = pd.DataFrame(df[df.state == state])
 		new_df = new_df.append(to_append)
 
-	
-	fig = px.bar(new_df, x="state", y="cases", color="state")
+	fig = go.Figure()
+	fig.add_trace(go.Bar(x=new_df['state'],
+                y=new_df['cases'],
+                name='Cases by State',
+                marker_color='rgb(55, 83, 109)'
+                ))
+	fig.add_trace(go.Bar(x=new_df['state'],
+                y=new_df['deaths'],
+                name='Deaths by State',
+                marker_color='rgb(26, 118, 255)'
+                ))
+
+	fig.update_layout(
+    title='Cases and Death Count Per State'
+    ,xaxis_tickfont_size=14,
+    yaxis=dict(
+        title='State',
+        titlefont_size=16,
+        tickfont_size=14,
+    ),
+    legend=dict(
+       
+        bgcolor='rgba(255, 255, 255, 0)',
+        bordercolor='rgba(255, 255, 255, 0)'
+    ),
+    barmode='group',
+    bargap=0.15, # gap between bars of adjacent location coordinates.
+    bargroupgap=0.1 # gap between bars of the same location coordinate.
+	)
+
+
+	#fig = px.bar(new_df, x="state", y="cases", color="state")
 	fig.update_layout(transition_duration=500)
 	return fig
 
